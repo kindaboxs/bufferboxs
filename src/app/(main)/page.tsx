dda7c32c49bf7991fc6dfb8608bdb9d0d4859d93
MrBoxs <rizkywanda200155@gmail.com>
2025-05-17 01:32:37 +0700

    feat: add GetOutPage component for under construction notice and redirect logic

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,10 +1,13 @@
 import { headers as getHeaders } from "next/headers";
+import { redirect } from "next/navigation";
 
 import { SignoutButton } from "@/components/global/signout-button";
 import { auth } from "@/lib/auth";
 
 export default async function HomePage() {
 	const session = await auth.api.getSession({ headers: await getHeaders() });
+
+	if (!session) redirect("/get-out");
 
 	return (
 		<div>
