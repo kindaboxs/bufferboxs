743e9198b959a412921efb41a7fe317143cdd4d8
MrBoxs <rizkywanda200155@gmail.com>
2025-05-18 23:19:39 +0700

    feat: wrap children in ScrollArea for improved layout and scrolling behavior

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,5 +1,6 @@
 import { ProtectedSidebar } from "@/app/(protected)/_components/protected-sidebar";
 import { SiteProtectedHeader } from "@/app/(protected)/_components/site-protected-header";
+import { ScrollArea } from "@/components/ui/scroll-area";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 
 export default function ProtectedLayout({
@@ -15,7 +16,14 @@ export default function ProtectedLayout({
 				<SiteProtectedHeader />
 				<div className="flex flex-1 flex-col">
 					<div className="@container/main flex flex-1 flex-col gap-2">
-						<main className="flex flex-col gap-4">{children}</main>
+						<main className="flex flex-col gap-4 p-4">
+							<ScrollArea
+								type="auto"
+								className="h-[calc(100dvh-5rem)] md:h-[calc(100dvh-6rem)]"
+							>
+								{children}
+							</ScrollArea>
+						</main>
 					</div>
 				</div>
 			</SidebarInset>
